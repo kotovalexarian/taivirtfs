944bc907a3b2a0a0480b0f6e823627fbeeda95f9
Alex Kotov <kotovalexarian@gmail.com>
2020-12-18 20:09:59 +0500

    Move commands into separate methods

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,22 @@ static const struct cuse_lowlevel_ops cuse_lowlevel_ops = {
     .poll      = nullptr,
 };
 
+static void command_inspect(
+    fuse_req_t req,
+    void *arg,
+    const void *in_buf,
+    size_t in_buf_size,
+    size_t out_buf_size
+);
+
+static void command_mount(
+    fuse_req_t req,
+    void *arg,
+    const void *in_buf,
+    size_t in_buf_size,
+    size_t out_buf_size
+);
+
 int main(int argc, char **argv)
 {
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
@@ -119,8 +135,6 @@ void on_ioctl(
     const size_t in_buf_size,
     const size_t out_buf_size
 ) {
-    const struct fuse_ctx *const ctx = fuse_req_ctx(req);
-
     if (flags & FUSE_IOCTL_COMPAT) {
         fuse_reply_err(req, ENOSYS);
         return;
@@ -128,85 +142,103 @@ void on_ioctl(
 
     switch (cmd) {
     case TAIVIRTFS_COMMAND_INSPECT:
-        {
-            if (out_buf_size == 0) {
-                virtual_file_system_inspect = virtual_file_system.inspect();
-
-                struct iovec out_iov = {
-                    arg,
-                    virtual_file_system_inspect.size()
-                };
-
-                fuse_reply_ioctl_retry(req, nullptr, 0, &out_iov, 1);
-            }
-            else {
-                fuse_reply_ioctl(
-                    req,
-                    0,
-                    virtual_file_system_inspect.c_str(),
-                    virtual_file_system_inspect.size()
-                );
-            }
-        }
+        command_inspect(req, arg, in_buf, in_buf_size, out_buf_size);
         break;
     case TAIVIRTFS_COMMAND_MOUNT:
-        {
-            if (in_buf_size == 0) {
-                const struct iovec in_iov[4] = {
-                    {
-                        .iov_base = (uint8_t*)arg + offsetof(
-                            struct TaiVirtFS_Command_Mount,
-                            target
-                        ),
-                        .iov_len = sizeof(char[256]),
-                    },
-                    {
-                        .iov_base = (uint8_t*)arg + offsetof(
-                            struct TaiVirtFS_Command_Mount,
-                            source
-                        ),
-                        .iov_len = sizeof(char[256]),
-                    },
-                    {
-                        .iov_base = (uint8_t*)arg + offsetof(
-                            struct TaiVirtFS_Command_Mount,
-                            file_system_type
-                        ),
-                        .iov_len = sizeof(char[256]),
-                    },
-                    {
-                        .iov_base = (uint8_t*)arg + offsetof(
-                            struct TaiVirtFS_Command_Mount,
-                            flags
-                        ),
-                        .iov_len = sizeof(unsigned long),
-                    }
-                };
-
-                fuse_reply_ioctl_retry(req, in_iov, 4, nullptr, 0);
-            }
-            else {
-                const struct TaiVirtFS_Command_Mount *const args =
-                    static_cast<const struct TaiVirtFS_Command_Mount*>(in_buf);
-
-                MountRequest mount_request(
-                    ctx->pid,
-                    ctx->uid,
-                    ctx->gid,
-                    args->target,
-                    args->source,
-                    args->file_system_type,
-                    args->flags
-                );
-
-                std::cout << mount_request.inspect();
-
-                int result = virtual_file_system.mount(mount_request);
-                fuse_reply_ioctl(req, result, nullptr, 0);
-            }
-        }
+        command_mount(req, arg, in_buf, in_buf_size, out_buf_size);
         break;
     default:
         fuse_reply_err(req, EINVAL);
+    }
+}
+
+void command_inspect(
+    fuse_req_t req,
+    void *const arg,
+    const void *const in_buf __attribute__((unused)),
+    const size_t in_buf_size __attribute__((unused)),
+    const size_t out_buf_size
+) {
+    if (out_buf_size == 0) {
+        virtual_file_system_inspect = virtual_file_system.inspect();
+
+        struct iovec out_iov = {
+            arg,
+            virtual_file_system_inspect.size()
+        };
+
+        fuse_reply_ioctl_retry(req, nullptr, 0, &out_iov, 1);
+    }
+    else {
+        fuse_reply_ioctl(
+            req,
+            0,
+            virtual_file_system_inspect.c_str(),
+            virtual_file_system_inspect.size()
+        );
+    }
+}
+
+void command_mount(
+    fuse_req_t req,
+    void *const arg,
+    const void *const in_buf,
+    const size_t in_buf_size,
+    const size_t out_buf_size __attribute__((unused))
+) {
+    if (in_buf_size == 0) {
+        const struct iovec in_iov[4] = {
+            {
+                .iov_base = (uint8_t*)arg + offsetof(
+                    struct TaiVirtFS_Command_Mount,
+                    target
+                ),
+                .iov_len = sizeof(char[256]),
+            },
+            {
+                .iov_base = (uint8_t*)arg + offsetof(
+                    struct TaiVirtFS_Command_Mount,
+                    source
+                ),
+                .iov_len = sizeof(char[256]),
+            },
+            {
+                .iov_base = (uint8_t*)arg + offsetof(
+                    struct TaiVirtFS_Command_Mount,
+                    file_system_type
+                ),
+                .iov_len = sizeof(char[256]),
+            },
+            {
+                .iov_base = (uint8_t*)arg + offsetof(
+                    struct TaiVirtFS_Command_Mount,
+                    flags
+                ),
+                .iov_len = sizeof(unsigned long),
+            }
+        };
+
+        fuse_reply_ioctl_retry(req, in_iov, 4, nullptr, 0);
+    }
+    else {
+        const struct fuse_ctx *const ctx = fuse_req_ctx(req);
+
+        const struct TaiVirtFS_Command_Mount *const args =
+            static_cast<const struct TaiVirtFS_Command_Mount*>(in_buf);
+
+        MountRequest mount_request(
+            ctx->pid,
+            ctx->uid,
+            ctx->gid,
+            args->target,
+            args->source,
+            args->file_system_type,
+            args->flags
+        );
+
+        std::cout << mount_request.inspect();
+
+        int result = virtual_file_system.mount(mount_request);
+        fuse_reply_ioctl(req, result, nullptr, 0);
     }
 }
